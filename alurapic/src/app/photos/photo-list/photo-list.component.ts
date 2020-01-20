f2647048f1744f5d6011a1f065210aa4351b147a
andregesser95 <andre.alfonsog@gmail.com>
2020-01-20 18:30:31 -0300

    Padrão debounce com RxJS

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subject } from "rxjs";
+import { debounceTime } from "rxjs/operators";
+
+import { Photo } from "../photo/photo";
 
 @Component({
-  selector: 'app-photo-list',
-  templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.css']
+  selector: "ap-photo-list",
+  templateUrl: "./photo-list.component.html",
+  styleUrls: ["./photo-list.component.css"]
 })
-export class PhotoListComponent implements OnInit {
-
-  photos: any[] = [];
-
-  constructor(private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute) { }
-
+export class PhotoListComponent implements OnInit, OnDestroy {
+  photos: Photo[] = [];
+  filter: string = "";
+  debounce: Subject<string> = new Subject<string>();
+  
+  constructor(private activatedRoute: ActivatedRoute) {}
+  
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => {
-        console.log(photos[0].userId)
-        this.photos = photos
-      });
+    this.photos = this.activatedRoute.snapshot.data["photos"];
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => (this.filter = filter));
+  }
+  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
